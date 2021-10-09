71c867db0416ca2aaecb0a032f8243c495078b77
John Lomma <lommaj@gmail.com>
2021-10-08 21:19:31 -0500

    adding timeline and lend details page

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,6 @@
 import Head from 'next/head'
-/* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import MainLayout from 'components/main-layout'
 import LoanList from 'components/loan-list.js'
-
-const user = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
-  imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
-const navigation = [
-  { name: 'Loans', href: '#', current: true },
-  { name: 'Repayments', href: '#', current: false },
-  { name: 'Information', href: '#', current: false },
-]
-const userNavigation = [
-  { name: 'Your Profile', href: '#' },
-  { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '#' },
-]
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
 
 export default function Home() {
   return (
@@ -35,108 +11,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>
-      <div className="bg-gray-800 pb-32">
-        <Disclosure as="nav" className="bg-gray-800">
-          {({ open }) => (
-            <>
-              <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div className="border-b border-gray-700">
-                  <div className="flex items-center justify-between h-16 px-4 sm:px-0">
-                    <div className="flex items-center">
-                      <div className="flex-shrink-0">
-                        <img
-                          className="h-8 w-8"
-                          src="ergo-dark.png"
-                          alt="Workflow"
-                        />
-                      </div>
-                      <div className="pl-3">
-                      <h1 className="text-white">Ergo Lend</h1>
-                      </div>
-                      <div className="hidden md:block">
-                        <div className="ml-10 flex items-baseline space-x-4">
-                          {navigation.map((item) => (
-                            <a
-                              key={item.name}
-                              href={item.href}
-                              className={classNames(
-                                item.current
-                                  ? 'bg-gray-900 text-white'
-                                  : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                'px-3 py-2 rounded-md text-sm font-medium'
-                              )}
-                              aria-current={item.current ? 'page' : undefined}
-                            >
-                              {item.name}
-                            </a>
-                          ))}
-                        </div>
-                      </div>
-                    </div>
-                    <div className="hidden md:block">
-                      <div className="ml-4 flex items-center md:ml-6">
-                        <button
-                          type="button"
-                          className="bg-gray-800 p-1 text-gray-400 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                        >
-                          <span className="sr-only">View notifications</span>
-                          <BellIcon className="h-6 w-6" aria-hidden="true" />
-                        </button>
-                      </div>
-                    </div>
-                    <div className="-mr-2 flex md:hidden">
-                      {/* Mobile menu button */}
-                      <Disclosure.Button className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                        <span className="sr-only">Open main menu</span>
-                        {open ? (
-                          <XIcon className="block h-6 w-6" aria-hidden="true" />
-                        ) : (
-                          <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                        )}
-                      </Disclosure.Button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <Disclosure.Panel className="border-b border-gray-700 md:hidden">
-                <div className="px-2 py-3 space-y-1 sm:px-3">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className={classNames(
-                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                        'block px-3 py-2 rounded-md text-base font-medium'
-                      )}
-                      aria-current={item.current ? 'page' : undefined}
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-              </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
-        <header className="py-10">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold text-white">Ergo Lend: Loans</h1>
-          </div>
-        </header>
-      </div>
-
-      <main className="-mt-32">
-        <div className="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
-          {/* Replace with your content */}
-          <div className="bg-white rounded-lg shadow px-5 py-6 sm:px-6">
-           <LoanList />
-          </div>
-          {/* /End replace */}
-        </div>
-      </main>
-    </div>
+        <MainLayout>
+          <LoanList />
+        </MainLayout>
     </div>
   )
 }
---
+++ pages/loan/[loan].js
@@ -0,0 +1,223 @@
+import { Fragment } from 'react'
+import { Menu, Popover, Transition } from '@headlessui/react'
+import {
+  ArrowNarrowLeftIcon,
+  CheckIcon,
+  HomeIcon,
+  PaperClipIcon,
+  QuestionMarkCircleIcon,
+  SearchIcon,
+  ThumbUpIcon,
+  UserIcon,
+} from '@heroicons/react/solid'
+import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import MiddleEllipsis from "react-middle-ellipsis";
+import MainLayout from 'components/main-layout'
+
+const user = {
+  name: 'Whitney Francis',
+  email: 'whitney@example.com',
+  imageUrl:
+    'https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80',
+}
+const navigation = [
+  { name: 'Dashboard', href: '#' },
+  { name: 'Jobs', href: '#' },
+  { name: 'Applicants', href: '#' },
+  { name: 'Company', href: '#' },
+]
+const breadcrumbs = [
+  { name: 'Jobs', href: '#', current: false },
+  { name: 'Front End Developer', href: '#', current: false },
+  { name: 'Applicants', href: '#', current: true },
+]
+const userNavigation = [
+  { name: 'Your Profile', href: '#' },
+  { name: 'Settings', href: '#' },
+  { name: 'Sign out', href: '#' },
+]
+const attachments = [
+  { name: 'resume_front_end_developer.pdf', href: '#' },
+  { name: 'coverletter_front_end_developer.pdf', href: '#' },
+]
+const eventTypes = {
+  applied: { icon: UserIcon, bgColorClass: 'bg-gray-400' },
+  advanced: { icon: ThumbUpIcon, bgColorClass: 'bg-blue-500' },
+  completed: { icon: CheckIcon, bgColorClass: 'bg-green-500' },
+}
+const timeline = [
+  {
+    id: 1,
+    type: eventTypes.applied,
+    content: 'created loan address',
+    target: '9gb3mPXuUbhVJrzvzR79d8hK5JTEWPduFfab4CkCFpr98dzQceG',
+    date: 'Sep 20',
+    datetime: '2020-09-20',
+  },
+  {
+    id: 2,
+    type: eventTypes.advanced,
+    content: 'Add funds of 3 Erg by',
+    target: '9ggCQdpJPSV9yekY8Mt8T1F2NmzEheY5egMZ7Z3ZXfUyziQe8Ey',
+    date: 'Sep 22',
+    datetime: '2020-09-22',
+  },
+  {
+    id: 3,
+    type: eventTypes.advanced,
+    content: 'Add funds of 3 Erg by',
+    target: '9ggCQdpJPSV9yekY8Mt8T1F2NmzEheY5egMZ7Z3ZXfUyziQe8Ey',
+    date: 'Sep 28',
+    datetime: '2020-09-28',
+  },
+  {
+    id: 4,
+    type: eventTypes.advanced,
+    content: 'Add funds of 3 Erg by',
+    target: '9ggCQdpJPSV9yekY8Mt8T1F2NmzEheY5egMZ7Z3ZXfUyziQe8Ey',
+    date: 'Sep 30',
+    datetime: '2020-09-30',
+  },
+  {
+    id: 5,
+    type: eventTypes.completed,
+    content: 'Loan Funded! Paid to',
+    target: '9hyTTDyCKBNKX2ZvFnuUkYmuEsMJYz3gQEyfAjSfsLRS2Yoiwbv',
+    date: 'Oct 4',
+    datetime: '2020-10-04',
+  },
+]
+const comments = [
+  {
+    id: 1,
+    name: 'Leslie Alexander',
+    date: '4d ago',
+    imageId: '1494790108377-be9c29b29330',
+    body:
+      'Ducimus quas delectus ad maxime totam doloribus reiciendis ex. Tempore dolorem maiores. Similique voluptatibus tempore non ut.',
+  },
+  {
+    id: 2,
+    name: 'Michael Foster',
+    date: '4d ago',
+    imageId: '1519244703995-f4e0f30006d5',
+    body:
+      'Et ut autem. Voluptatem eum dolores sint necessitatibus quos. Quis eum qui dolorem accusantium voluptas voluptatem ipsum. Quo facere iusto quia accusamus veniam id explicabo et aut.',
+  },
+  {
+    id: 3,
+    name: 'Dries Vincent',
+    date: '4d ago',
+    imageId: '1506794778202-cad84cf45f1d',
+    body:
+      'Expedita consequatur sit ea voluptas quo ipsam recusandae. Ab sint et voluptatem repudiandae voluptatem et eveniet. Nihil quas consequatur autem. Perferendis rerum et.',
+  },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Example() {
+  return (
+    <MainLayout>
+    <div className="relative min-h-screen bg-gray-100">
+      <main className="py-10">
+        {/* Page header */}
+        <div className="max-w-3xl mx-auto px-4 sm:px-6 md:flex md:items-center md:justify-between md:space-x-5 lg:max-w-7xl lg:px-8">
+          <div className="flex items-center space-x-5">
+            <div>
+              <h1 className="text-2xl font-bold text-gray-900">Loan 1</h1>
+              <p className="text-sm font-medium text-gray-500">
+                Created on <time dateTime="2020-08-25">October 8, 2021</time>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-8 max-w-3xl mx-auto grid grid-cols-1 gap-6 sm:px-6 lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-3">
+          <div className="space-y-6 lg:col-start-1 lg:col-span-2">
+            {/* Description list*/}
+            <section aria-labelledby="applicant-information-title">
+              <div className="bg-white shadow sm:rounded-lg">
+                <div className="px-4 py-5 sm:px-6">
+                  <h2 id="applicant-information-title" className="text-lg leading-6 font-medium text-gray-900">
+                    Applicant Information
+                  </h2>
+                  <p className="mt-1 max-w-2xl text-sm text-gray-500">Personal details and application.</p>
+                </div>
+                <div className="border-t border-gray-200 px-4 py-5 sm:px-6">
+                  <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+                    <div className="sm:col-span-2">
+                      <dt className="text-sm font-medium text-gray-500">About</dt>
+                      <dd className="mt-1 text-sm text-gray-900">
+                        Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat.
+                        Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia
+                        proident. Irure nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
+                      </dd>
+                    </div>
+                  </dl>
+                </div>
+              </div>
+            </section>
+          </div>
+
+          <section aria-labelledby="timeline-title" className="lg:col-start-3 lg:col-span-1">
+            <div className="bg-white px-4 py-5 shadow sm:rounded-lg sm:px-6">
+              <h2 id="timeline-title" className="text-lg font-medium text-gray-900">
+                Timeline
+              </h2>
+
+              {/* Activity Feed */}
+              <div className="mt-6 flow-root">
+                <ul role="list" className="-mb-8">
+                  {timeline.map((item, itemIdx) => (
+                    <li key={item.id}>
+                      <div className="relative pb-8">
+                        {itemIdx !== timeline.length - 1 ? (
+                          <span className="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200" aria-hidden="true" />
+                        ) : null}
+                        <div className="relative flex space-x-3">
+                          <div>
+                            <span
+                              className={classNames(
+                                item.type.bgColorClass,
+                                'h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white'
+                              )}
+                            >
+                              <item.type.icon className="w-5 h-5 text-white" aria-hidden="true" />
+                            </span>
+                          </div>
+                          <div className="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
+                            <div>
+                              <p className="text-sm text-gray-500">
+                                {item.content}{' '}
+                              </p>
+                              <div style={{width:"140px"}}><MiddleEllipsis><span>{item.target}</span></MiddleEllipsis></div>
+                            </div>
+                            <div className="text-right text-sm whitespace-nowrap text-gray-500">
+                              <time dateTime={item.datetime}>{item.date}</time>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              {/* <div className="mt-6 flex flex-col justify-stretch">
+                <button
+                  type="button"
+                  className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                >
+                  Advance to offer
+                </button>
+              </div> */}
+            </div>
+          </section>
+        </div>
+      </main>
+    </div>
+    </MainLayout>
+  )
+}
