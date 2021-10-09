54fe2e508cfa1b2ec35bb0735f45249b3878a3bf
John Lomma <lommaj@gmail.com>
2021-10-09 15:17:16 -0500

    making entire block clickable.

--- components/loan-list.js
+++ components/loan-list.js
@@ -94,66 +94,78 @@ export default function LoanList() {
 
       <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
         {stats.map((item) => (
-          <div
+          <Link
             key={item.id}
-            className="relative bg-white pt-5 px-4 pb-12 sm:pt-6 sm:px-6 shadow rounded-lg overflow-hidden"
+            href={{
+              pathname: '/loan/[loan]',
+              query: { loan: item.id }
+            }}
           >
-            <dt>
-              <div className="absolute bg-indigo-500 rounded-md p-3">
-                <item.icon className="h-6 w-6 text-white" aria-hidden="true" />
-              </div>
-              <p className="ml-16 text-sm font-medium text-gray-500 truncate">
-                {item.name}
-              </p>
-            </dt>
-            <dd className="ml-16 pb-6 flex items-baseline sm:pb-7">
-              <p className="text-2xl font-semibold text-gray-900">
-                {item.stat}
-              </p>
-              <p
-                className={classNames(
-                  item.changeType === 'increase'
-                    ? 'text-green-600'
-                    : 'text-red-600',
-                  'ml-2 flex items-baseline text-sm font-semibold'
-                )}
-              >
-                {item.changeType === 'increase' ? (
-                  <ArrowSmUpIcon
-                    className="self-center flex-shrink-0 h-5 w-5 text-green-500"
-                    aria-hidden="true"
-                  />
-                ) : (
-                  <ArrowSmDownIcon
-                    className="self-center flex-shrink-0 h-5 w-5 text-red-500"
-                    aria-hidden="true"
-                  />
-                )}
-
-                <span className="sr-only">
-                  {item.changeType === 'increase' ? 'Increased' : 'Decreased'}{' '}
-                  by
-                </span>
-                {item.change}
-              </p>
-              <div className="absolute bottom-0 inset-x-0 bg-gray-50 px-4 py-4 sm:px-6">
-                <div className="text-sm">
-                  <Link
-                    href={{
-                      pathname: '/loan/[loan]',
-                      query: { loan: item.id }
-                    }}
+            <a>
+              <div className="relative bg-white pt-5 px-4 pb-12 sm:pt-6 sm:px-6 shadow rounded-lg overflow-hidden">
+                <dt>
+                  <div className="absolute bg-indigo-500 rounded-md p-3">
+                    <item.icon
+                      className="h-6 w-6 text-white"
+                      aria-hidden="true"
+                    />
+                  </div>
+                  <p className="ml-16 text-sm font-medium text-gray-500 truncate">
+                    {item.name}
+                  </p>
+                </dt>
+                <dd className="ml-16 pb-6 flex items-baseline sm:pb-7">
+                  <p className="text-2xl font-semibold text-gray-900">
+                    {item.stat}
+                  </p>
+                  <p
+                    className={classNames(
+                      item.changeType === 'increase'
+                        ? 'text-green-600'
+                        : 'text-red-600',
+                      'ml-2 flex items-baseline text-sm font-semibold'
+                    )}
                   >
-                    <a className="font-medium text-indigo-600 hover:text-indigo-500">
-                      {' '}
-                      View loan
-                      <span className="sr-only"> {item.name} stats</span>
-                    </a>
-                  </Link>
-                </div>
+                    {item.changeType === 'increase' ? (
+                      <ArrowSmUpIcon
+                        className="self-center flex-shrink-0 h-5 w-5 text-green-500"
+                        aria-hidden="true"
+                      />
+                    ) : (
+                      <ArrowSmDownIcon
+                        className="self-center flex-shrink-0 h-5 w-5 text-red-500"
+                        aria-hidden="true"
+                      />
+                    )}
+
+                    <span className="sr-only">
+                      {item.changeType === 'increase'
+                        ? 'Increased'
+                        : 'Decreased'}{' '}
+                      by
+                    </span>
+                    {item.change}
+                  </p>
+                  <div className="absolute bottom-0 inset-x-0 bg-gray-50 px-4 py-4 sm:px-6">
+                    <div className="text-sm">
+                      <Link
+                        href={{
+                          pathname: '/loan/[loan]',
+                          query: { loan: item.id }
+                        }}
+                      >
+                        <a className="font-medium text-indigo-600 hover:text-indigo-500">
+                          {' '}
+                          View loan
+                          <span className="sr-only"> {item.name} stats</span>
+                        </a>
+                      </Link>
+                    </div>
+                  </div>
+                </dd>
               </div>
-            </dd>
-          </div>
+            </a>
+          </Link>
         ))}
       </dl>
     </div>
